5bb7d94d0a298fb6cd6996bfa4fa955a74033bac
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2022-09-25 03:09:03 -0300

    fix: fixed User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,11 @@
 const UserSchema = (sequelize, DataTypes) => {
   const UserTable = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
