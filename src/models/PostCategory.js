3337f5abee813677bb16ea746e852787c2e14f7a
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2022-09-25 21:48:15 -0300

    chore: added PostCategory model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,28 @@
+const PostCategorySchema = (sequelize, DataTypes) => {
+  const PostCategoryTable = sequelize.define('PostCategory', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  }, {
+    tableName: 'posts_categories',
+    underscored: true,
+    timestamps: false,
+  });
+
+  PostCategoryTable.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategoryTable,
+      foreignKey: 'post_id',
+      otherKey: 'category_id'
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategoryTable,
+      foreignKey: 'category_id',
+      otherKey: 'post_id'
+    });
+  };
+  return PostCategoryTable;
+};
+
+module.exports = PostCategorySchema;
